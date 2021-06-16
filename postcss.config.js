4280e96184145ac7e14ae2b0aa70dd2be66cef5e
davidteren <drteren@gmail.com>
2021-06-16 08:48:50 +0200

    update postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,16 @@
 let environment = {
-  plugins: [
-    require('postcss-import'),
-    require('tailwindcss'),
-    require('autoprefixer'),
-    require('postcss-flexbugs-fixes'),
-  ]
+    plugins: [
+        require('postcss-import'),
+        require('autoprefixer'),
+        require('tailwindcss'),
+        require('postcss-flexbugs-fixes'),
+        require('postcss-preset-env')({
+            autoprefixer: {
+                flexbox: 'no-2009'
+            },
+            stage: 3
+        })
+    ]
 }
 
 if (process.env.RAILS_ENV === "production") {
