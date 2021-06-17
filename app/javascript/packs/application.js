31da79f2c6796e40c261f2ff302625651432f862
davidteren <drteren@gmail.com>
2021-06-17 14:49:32 +0200

    fixes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,10 +7,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "stylesheets/application.scss"
+import "stylesheets/application"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-
-
