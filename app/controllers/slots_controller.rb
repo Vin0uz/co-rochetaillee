6c63aaf967f505f7c2214b075e999b5118acdbe5
Kevin Vinhas <kevin@vinhas.fr>
2021-09-22 00:33:08 +0200

    Add form and naive registration.

--- app/controllers/slots_controller.rb
+++ app/controllers/slots_controller.rb
@@ -3,5 +3,17 @@ class SlotsController < ApplicationController
   end
 
   def create
+    slot = Slot.create(slot_params)
+    if slot.persisted?
+      redirect_to root_path, notice: "✅ On te contactera pour te donner des précisions sur le créneau si nécessaire"
+    else
+      redirect_to new_slot_path, alert: "Ça n'a pas marché.. Si le problème persiste, contacte nous sur facebook!"
+    end
+  end
+
+  private
+
+  def slot_params
+    params.require(:slot).permit(:first_name, :last_name, :email, :phone, :begin_at, :end_at, :duration)
   end
 end
